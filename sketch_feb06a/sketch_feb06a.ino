3ee5bcff3d9aee6cb193ce0d0a1fae8b5fd8f5b1
Leo Cui <59667106+CandidlyRed@users.noreply.github.com>
2023-02-06 20:47:48 -0600

    Roomba moves kekj

---
+++ sketch_feb06a/sketch_feb06a.ino
@@ -0,0 +1,119 @@
+#include "roombaDefines.h"
+#include <SoftwareSerial.h>
+
+int rxPin=10;
+int txPin=11;
+SoftwareSerial Roomba(rxPin,txPin);
+
+void setup() 
+{
+  Roomba.begin(19200);
+  
+  pinMode(ddPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP); // connected to Arduino pin 12 and used for "starting"
+
+  delay(2000);
+  
+  wakeUp();
+  startSafe();
+
+  drive(500, 0);
+}
+
+void loop() 
+{
+  
+}
+
+void wakeUp (void)
+{
+//  setWarningLED(ON);
+  digitalWrite(ddPin, HIGH);
+  delay(100);
+  digitalWrite(ddPin, LOW);
+  delay(500);
+  digitalWrite(ddPin, HIGH);
+  delay(2000);
+}
+
+void startSafe()
+{  
+  Roomba.write(128);  //Start
+  Roomba.write(131);  //Safe mode
+  delay(1000);
+}
+
+void drive(int velocity, int radius)
+{
+  clamp(velocity, -500, 500); //def max and min velocity in mm/s
+  clamp(radius, -2000, 2000); //def max and min radius in mm
+  
+  Roomba.write(137);
+  Roomba.write(velocity >> 8);
+  Roomba.write(velocity);
+  Roomba.write(radius >> 8);
+  Roomba.write(radius);
+}
+
+//---------------------------------------------------------------
+
+void driveWheels(int right, int left)
+{
+  clamp(right, -500, 500);
+  clamp(left, -500, 500);
+  
+  Roomba.write(145);
+  Roomba.write(right >> 8);
+  Roomba.write(right);
+  Roomba.write(left >> 8);
+  Roomba.write(left);
+  }
+
+//---------------------------------------------------------------
+void driveWheelsPWM(int rightPWM, int leftPWM)
+{
+  clamp(rightPWM, -255, 255);
+  clamp(leftPWM, -255, 255);
+  
+  Roomba.write(146);
+  Roomba.write(rightPWM >> 8);
+  Roomba.write(rightPWM);
+  Roomba.write(leftPWM >> 8);
+  Roomba.write(leftPWM);
+}
+
+//---------------------------------------------------------------
+void turnCW(unsigned short velocity, unsigned short degrees)
+{
+  drive(velocity, -1);
+  clamp(velocity, 0, 500);
+  delay(6600);
+  drive(0,0);
+}
+
+//---------------------------------------------------------------
+void turnCCW(unsigned short velocity, unsigned short degrees)
+{
+  drive(velocity, 1); 
+  clamp(velocity, 0, 500);
+  delay(6600);
+  drive(0,0);
+}
+
+//---------------------------------------------------------------
+void driveStop(void)
+{
+  drive(0,0);
+}
+
+//---------------------------------------------------------------
+void driveLeft(int left)
+{
+  driveWheels(left, 0);
+}
+
+//---------------------------------------------------------------
+void driveRight(int right)
+{
+  driveWheels(0, right);
+}
